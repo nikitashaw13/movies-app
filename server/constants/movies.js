c6127b8f93464ca4172a3dd0969a3c6ca41642c4
nikitashaw1 <nikita.shaw1@byjus.com>
2021-11-02 16:23:22 +0530

    movie-app

---
+++ server/constants/movies.js
@@ -0,0 +1,31 @@
+const movies = [{
+    id:1,
+    title : "Batman",
+    poster:'',
+    rating:9
+},{
+    id:2,
+    title : "Superman",
+    poster:'',
+    rating:9
+},{
+    id:3,
+    title : "Venom",
+    poster:'',
+    rating:9
+},{
+    id:4,
+    title : "Wonder Women",
+    poster:'',
+    rating:9
+},{
+    id:5,
+    title : "Spider-man",
+    poster:'',
+    rating:9
+}
+];
+
+module.exports = {
+    movies
+}
